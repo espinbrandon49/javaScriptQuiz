aa1c99f5ad698f5998ae933820335f24faab6094
Brandon Espinosa <espinbrandon49@gmail.com>
2022-06-25 15:32:39 -0700

    can keep score

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,18 +1,24 @@
-const answers = () => {
+const question1 = document.getElementById('cat')
+const correctAnswers = () => {
   let answer
-  if (document.querySelector('.question1').style.display == 'block') {
-    answer =='cat'
+  if (window.getComputedStyle(question1).display != "none") {
+    answer ='q1A'
   }
-  console.log(answer)
   return answer
 }
-answers()
 
+let count = 0
 function submitAnswer() {
+  const correctAnswer = correctAnswers()
   document.querySelectorAll('input').forEach((input) => {
-    if (input.checked == true && input.value == 'q1A') {
-       console.log(input.value)
+    if (input.checked == true && input.value == correctAnswer) {
+      count++
+      console.log(input.value)
     }
   })
+  console.log(count)
+  return count
 }
+
 document.querySelector('.button').addEventListener('click', submitAnswer)
+
